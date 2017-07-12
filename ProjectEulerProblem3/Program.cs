7bee35b5458d0f9ba1bd7aac788340d0946d1991
Alexander Aldokhin <alex.aldokhin@outlook.com>
2017-07-13 00:42:04 +0700

    Add project files.

---
+++ ProjectEulerProblem3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectEulerProblem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
